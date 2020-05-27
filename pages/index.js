9382a144dce9124fe80c04a8b19c0ae2ae86a6dc
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2020-05-27 12:56:45 +0100

    mobile responsiveness

--- pages/index.js
+++ pages/index.js
@@ -189,8 +189,8 @@ const Home = () => {
           @media (max-width: 459px) {
             .intro-cover {
               background: linear-gradient(
-                  rgba(0, 0, 0, 0.75),
-                  rgba(0, 0, 0, 0.75)
+                  rgba(0, 0, 0, 0.85),
+                  rgba(0, 0, 0, 0.85)
                 ),
                 url("me.png");
               width: 100%;
