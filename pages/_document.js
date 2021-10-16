67490124dc61fdb2882031ef62e0b3f1214e6f11
Adebisi Ahmed <adebisi.ahmed10@gmail.com>
2021-10-16 19:29:49 +0100

    feature: add google verification

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,9 @@ export default class MyDocument extends Document {
             name="viewport"
             content="width=device-width, initial-scale=1.0"
           />
+          <title>⭐️ Adebisi Ahmed</title>
           <meta name="Description" content="Adebisi Ahmed's Website"></meta>
+          <meta name="google-site-verification" content="SADsZJG4_2hV7Tviu2bTkY2CUFCtufFzcw6nQwHgN8E" />
           <link
             href="https://fonts.googleapis.com/css?family=Nunito:300,500,600,800&display=swap"
             rel="stylesheet"
